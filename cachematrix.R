eb1a068e6f1600152c60f506aad7d793ff64fb93
Vijay Darji <Vijay@Vijays-MBP.fios-router.home>
2018-12-12 17:14:28 -0500

    assignment completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Script to get the Inverse Matrix of a square Matrix.
+## Script it optimized to load the cached Inverse Matrix if the
+## Inverse was previously calculated
 
-## Write a short comment describing this function
+## This function creates the cached matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+      x <<- y
+      m <<- NULL
+  }
+  get <- function() x
+  setInverseMatrix <- function(inverseMatrix) m <<- inverseMatrix
+  getInverseMatrix <- function() m
+  list(set = set, get = get,
+       setInverseMatrix = setInverseMatrix,
+       getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## This function will load the inverse matrix from cache.
+## If the inverse matrix does not exist in cache, it will
+## load run solve() to calculate the inverse matrix of the
+## square matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverseMatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverseMatrix(m)
+  m
 }
